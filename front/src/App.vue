5953b6fb4462fd90daaf00922fcfca5a61c62f5c
daria-morgen <dk.957@ya.ru>
2023-10-10 02:03:14 +0300

    Добавлено графическое изображение карточек товаров, добавлена загрузка всех товаров при открытии страницы. Реализован поиск товара по имени.

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,31 +1,59 @@
 <template>
   <div class="head_div">
     <hr>
-    <ProductComponent></ProductComponent>
-    <span>{{ products }}</span>
-    <button @click="getList()">GetList</button>
+    <input class="search_input" v-model="searchValue"/>
+    <button @click="searchProduct()">Поиск</button>
+    <br>
+    <br>
+    <ul class="listOfProducts">
+      <li v-for="(product, index) in products" :key="index" class="product">
+        <h3 class="product-name">
+          {{ product.name }}
+        </h3>
+        <div class="product-price">
+          <span>{{ product.prise }}</span>
+          <!--        <span>10 x {{ Math.round(product.price / 10) }}, 00 </span>-->
+        </div>
+      </li>
+    </ul>
+    <hr>
 
   </div>
 </template>
 
 <script>
-import ProductComponent from './components/ProductComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    ProductComponent
+    // ProductComponent
   },
   data() {
     return {
+      searchValue: "",
       products: []
     }
   },
   methods: {
-    getList() {
+    getAllProducts() {
       this.axios.get('http://localhost:8081/products')
           .then(response => (this.products = response.data));
+
+      try {
+        this.products = JSON.parse(this.products)
+      } catch (error) {
+        return null;
+      }
+    },
+    searchProduct() {
+      const urlString = 'http://localhost:8081/productByName/' + this.searchValue
+
+      this.axios.get(urlString)
+          .then(response => (this.products = response.data));
     }
+  },
+  beforeMount() {
+    this.getAllProducts()
   }
 }
 </script>
@@ -38,5 +66,52 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.search_input {
+  margin-right: 10px;
+}
+
+.listOfProducts {
+  width: 100%;
+  max-width: 1000px;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  padding: 0;
+}
+
+.product {
+  width: 300px;
+  background-color: #fff;
+  list-style: none;
+  box-sizing: border-box;
+  padding: 1em;
+  margin: 1em 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 7px;
+  /*border-color: black;*/
+  border: solid;
+}
+
+.product-name {
+  font-size: 1.2em;
+  font-weight: normal;
+}
+
+/*.product-name:hover {*/
+/*  cursor: pointer;*/
+/*  text-decoration: underline;*/
+/*}*/
+
+.product-price {
+  width: 100%;
+  align-self: flex-start;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: .5em;
 }
 </style>
